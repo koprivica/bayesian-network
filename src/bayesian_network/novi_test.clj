f51e522ff55e02641df1b64353c8261723792ba7
marko-k <marko.koprivica@hotmail.com>
2013-09-20 10:03:33 +0200

    Signed-off-by: marko-k <marko.koprivica@hotmail.com>

--- src/bayesian_network/novi_test.clj
+++ src/bayesian_network/novi_test.clj
@@ -2,7 +2,26 @@
  ; (:use [incanter core charts stats])
   )
 
-(defrecord Node [node-name node-state node-parents])
+(defrecord Node [^String node-name node-parents node-output])
+
+(defrecord InputNode [^String node-name node-state nod-probability] )
 
 (defprotocol NodeDriving
-  (probability [node forebears]))
+  "Protocol for managing nodes."
+  (probability [this forebears distribution])
+  (node-state [this forebears distribution]))
+
+
+(extend-protocol NodeDriving
+  Node
+  (probability [this]
+    (let [par
+     (loop [sek seq(this)]
+      (if (and sek sek)
+        (if (= (:node-output (first (first sek))) par
+
+
+      )
+    )
+
+  )
